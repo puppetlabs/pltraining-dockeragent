aa55810dc84c38173f269250d6b7d6bb7b98c20f
Josh Samuelson <js@puppetlabs.com>
2015-10-01 12:45:38 -0700

    Enable init system on containers

--- manifests/node.pp
+++ manifests/node.pp
@@ -9,7 +9,6 @@ define dockeragent::node (
     image            => 'agent',
     command          => '/sbin/init 3',
     use_name         => true,
-    privileged       => true,
     ports            => $ports,
     volumes          => $dockeragent::container_volumes,
     extra_parameters => [
@@ -18,4 +17,9 @@ define dockeragent::node (
     ],
   }
 
+  exec { "docker exec -d ${title} /sbin/init 3":
+    path    => ['/usr/bin','/bin'],
+    unless  => "docker exec ${title} runlevel | grep '3$'",
+    require => Docker::Run[$title],
+  }
 }
