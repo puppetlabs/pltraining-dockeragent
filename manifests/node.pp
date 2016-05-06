00cd407ab7fe7446b71d59e46b07c8aa6833c547
kjhenner <kjhenner@gmail.com>
2016-05-06 11:18:30 -0700

    add remove_container_on_start/stop params

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,15 +1,19 @@
 # Create and run a Docker containerized Puppet Agent
 define dockeragent::node (
   $ports = undef,
+  $remove_container_on_start = true,
+  $remove_container_on_stop = true
 ) {
   require dockeragent
 
   docker::run { $title:
-    hostname         => $title,
-    image            => 'agent',
-    command          => '/usr/lib/systemd/systemd',
-    ports            => $ports,
-    volumes          => $dockeragent::container_volumes,
+    hostname                  => $title,
+    image                     => 'agent',
+    command                   => '/usr/lib/systemd/systemd',
+    ports                     => $ports,
+    volumes                   => $dockeragent::container_volumes,
+    remove_container_on_start => $remove_container_on_start,
+    remove_container_on_stop  => $remove_container_on_stop,
     extra_parameters => [
       "--add-host \"${::fqdn} puppet:${::ipaddress_docker0}\"",
       '--restart=always',
