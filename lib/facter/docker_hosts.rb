008744cc34eaa724166a59231d014ed6cec5d421
kjhenner <kjhenner@gmail.com>
2016-08-25 15:04:05 -0700

    make docker_hosts fact return an empty hash if docker isn't installed

--- lib/facter/docker_hosts.rb
+++ lib/facter/docker_hosts.rb
@@ -1,6 +1,10 @@
 Facter.add(:docker_hosts) do
   setcode do
-    containers = `docker ps | awk '{if(NR>1) print $NF}'`.split
-    containers.inject({}){ |memo, container| memo[container] = `/bin/docker inspect -f '{{ .NetworkSettings.IPAddress }}' #{container}`.chomp; memo }
+    if system("docker -v")
+      containers = `docker ps | awk '{if(NR>1) print $NF}'`.split
+      return containers.inject({}){ |memo, container| memo[container] = `/bin/docker inspect -f '{{ .NetworkSettings.IPAddress }}' #{container}`.chomp; memo }
+    else
+      return {}
+    end
   end
 end
